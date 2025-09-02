0322ce22efab26616296fd6f34453d62d0b97105
Omshi255 <swati356ssen@gmail.com>
2025-09-02 17:42:37 +0530

    Initial commit - added javascript_task01

---
+++ javascript_task01/script.js
@@ -0,0 +1,20 @@
+/* 1.  Create a js file which contains variable declaration for a form which asks following basic details :
+(What is your Name, Email, age, address, phone number, gender, Are you married)
+2. Add comments on the top of the file to note this question as well.*/
+
+let  name  = "";
+let  email = "";
+let  address="";
+let  phoneNumber = "";
+let gender="";
+let isMarried = false;
+
+console.log(name);
+console.log(email);
+console.log(address);
+console.log(phoneNumber);
+console.log(gender);
+console.log(isMarried);
+
+
+
