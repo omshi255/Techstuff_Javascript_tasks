aa1cd8983b3a8bda0496e1749cb6bc4aa7a266ec
Omshi255 <swati356ssen@gmail.com>
2025-09-03 15:18:49 +0530

    added javascript_task03

---
+++ javascript_task03/readme.md
@@ -0,0 +1,5 @@
+### Question:
+```
+1. In the variables declare above assign the variables as appropriate default value instead of null.
+
+```
---
+++ javascript_task03/script.js
@@ -0,0 +1,29 @@
+let number = 2;
+console.log(number);
+console.log(typeof(number));
+
+let big = 0n;
+console.log(big);
+console.log(typeof(big));
+
+let  str = "OMSHREEE";
+console.log(str);
+console.log(typeof(str));
+
+
+let isActive = false;
+console.log(isActive);
+console.log(typeof(isActive));
+
+let notAssigned;
+console.log(notAssigned);
+
+let emptyValue = null;
+console.log(emptyValue);
+
+let  uniqueId = Symbol();
+console.log(uniqueId);
+console.log(typeof(uniqueId));
+
+
+
